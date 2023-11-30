3f446a446e03669b57d52e33f98406bf2f762c5e
Charles Bromley <charles.bromley@outlook.com>
2023-11-30 11:53:36 +0000

    Unsuccessful in implementing different home pages for logged in and logged out

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-from .views import Index
+from .views import Index, LoggedInHome, HomeRedirectView
 
 urlpatterns = [
-    path('', Index.as_view(), name= 'home')
+    path('', Index.as_view(), name='home_logged_out'),
+    path('logged-in-home/', LoggedInHome.as_view(), name='logged_in_home'),
+    path('home-redirect/', HomeRedirectView.as_view(), name='home_redirect'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, RedirectView
+from django.urls import reverse_lazy
 
 
 class Index(TemplateView):
     template_name = 'home/index.html'
+
+class LoggedInHome(LoginRequiredMixin, TemplateView):
+    template_name= 'home/logged-in-home.html'
+    login_url = reverse_lazy('home_logged_out')
+
+class HomeRedirectView(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return reverse_lazy('logged_in_home')
+        else:
+            return reverse_lazy('home_logged_out')
