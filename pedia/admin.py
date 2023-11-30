7cdde2d5d765ab50165cb5cc196c6cbb6fc3c218
Charles Bromley <charles.bromley@outlook.com>
2023-11-30 12:47:02 +0000

    Add Article model and amin register in pedia fiolder

--- pedia/admin.py
+++ pedia/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'animal_name',
+        'location',
+        'diet',
+        'description',
+        'post_date',
+    )
--- pedia/models.py
+++ pedia/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    """
+    A model for superuser to create, read, update, and delete articles
+    """
+    user = models.ForeignKey(User, related_name="author", on_delete=models.CASCADE)
+    animal_name = models.CharField(max_length=50, null=False, blank=False)
+    location = models.CharField(max_length=50, null=False, blank=False)
+    diet = models.CharField(max_length=30, null=False, blank=False)
+    description = models.TextField(max_length=200, null=False, blank=False)
+    post_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['post_date']
+
+    def __str__(self):
+        return str(self.animal_name)
