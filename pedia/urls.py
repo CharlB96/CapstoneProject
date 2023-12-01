49c685874ea10609c6117d8851a538427a312174
Charles Bromley <charles.bromley@outlook.com>
2023-12-01 15:40:59 +0000

    Add article_detail template, add url and views for page to link correctly

--- pedia/urls.py
+++ pedia/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
-from .views import article_view
+from .views import article_view, article_detail
 
 urlpatterns = [
-    path('', article_view, name='article_view')
+    path('', article_view, name='article_view'),
+    path('<int:article_id>/', article_detail, name='article_detail')
 ]
--- pedia/views.py
+++ pedia/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 
 def article_view(request):
     article_items = Article.objects.all()
     return render(request, 'pedia/pedia.html', {'article_items': article_items})
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'pedia/article_detail.html', {'article': article})
